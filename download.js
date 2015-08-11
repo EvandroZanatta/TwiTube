b261687b4bb92ecf3a47fbb794eee1773b27f26d
Evandro Zanatta <hemanstifler@gmail.com>
2015-08-11 16:51:55 +0000

    Realizando download e convertendo para audio

--- download.js
+++ download.js
@@ -1,4 +1,13 @@
-var Twit = require('twit')
+var Twit = require('twit');
+var exec = require('child_process').exec, child;
+/*
+var redis = require("redis"),
+    client = redis.createClient();
+    
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+*/
 
 var T = new Twit({
     consumer_key:         'r0byHfbTJzCTlLhmGRIu5IO0p'
@@ -7,6 +16,30 @@ var T = new Twit({
   , access_token_secret:  'BGlrk0QB5EXgcyfhz8MqTV4Aey8JYMIEtZ2QAhgLxknyH'
 })
 
-T.get('search/tweets', { q: 'youtube.com', count: 1 }, function(err, data, response) {
-  console.log(data.statuses[0].entities.urls[0].expanded_url);
+T.get('search/tweets', { q: 'youtube.com from:sonteam', count: 10 }, function(err, data, response) {
+  //console.log(data.statuses[0].entities.urls[0].expanded_url);
+  /*for (var i in data.statuses) {
+    var val = data.statuses[i];
+    console.log(val+'\n');
+  }*/
+  //console.log(data.statuses.id);
+  //console.log(data.statuses[0].entities);
+  console.log(data.statuses[0].entities.urls[0]);
+    
+  process.chdir('music/');
+  
+  child = exec('youtube-dl --extract-audio --audio-format mp3 '+data.statuses[0].entities.urls[0].expanded_url,
+    function (error, stdout, stderr) {
+      console.log('stdout: ' + stdout);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+  });
+  
+  /*
+  process.chdir('music/');
+  console.log(process.cwd());
+  process.chdir('../');
+  console.log(process.cwd());
+  */
 });
