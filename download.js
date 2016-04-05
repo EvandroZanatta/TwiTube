e482c88e8fcc2d3bc6e3ac9210cf68dc4d093586
Evandro Zanatta <hemanstifler@gmail.com>
2016-04-05 19:33:50 +0000

    Remodelando projeto

--- download.js
+++ download.js
@@ -1,54 +1,41 @@
-var Twit = require('twit');
+console.log('Iniciando...');
 var exec = require('child_process').exec, child;
-/*
-var redis = require("redis"),
-    client = redis.createClient();
-    
-client.on("error", function (err) {
-    console.log("Error " + err);
+
+var express = require('express');
+var app = express();
+app.use('/music', express.static('music'));
+app.use('/static', express.static('view/static'));
+
+app.get('/', function(req, res) {
+    res.sendfile('view/index.html');
 });
-*/
 
-var T = new Twit({
-    consumer_key:         'r0byHfbTJzCTlLhmGRIu5IO0p'
-  , consumer_secret:      'noNbKJYJfOBoxcz70Mt1LBialrGWuKSJqJgsOesASZ2prOjlhE'
-  , access_token:         '158037102-sNcp5MfsXmYslUFWJ9hgLTx2j3p1YqoEeOIZRgsp'
-  , access_token_secret:  'BGlrk0QB5EXgcyfhz8MqTV4Aey8JYMIEtZ2QAhgLxknyH'
-})
-
-process.chdir('music/');
-
-T.get('search/tweets', { q: 'youtube.com musica lang:pt', count: 5 }, function(err, data, response) {
-  for (var i in data.statuses) {
+app.get('/dowload/*', function(req, res) {
     
-    var val = data.statuses[i].user.name;
-    console.log('Baixando a musica de '+val);
+    var link = req.query.link;
     
-    child = exec('youtube-dl --extract-audio --audio-format mp3 '+data.statuses[i].entities.urls[0].expanded_url,
-    function (error, stdout, stderr) {
-      //console.log('stdout: ' + stdout);
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      }
-    });
-  }
-  //console.log(data.statuses[0].user);
-  //console.log(data.statuses[0].entities);
-  //console.log(data.statuses[0].entities.urls[0]);
-  
-  /*child = exec('youtube-dl --extract-audio --audio-format mp3 '+data.statuses[0].entities.urls[0].expanded_url,
-    function (error, stdout, stderr) {
-      //console.log('stdout: ' + stdout);
-      console.log('Baixando a musica');
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      }
-  });*/
-  
-  /*
-  process.chdir('music/');
-  console.log(process.cwd());
-  process.chdir('../');
-  console.log(process.cwd());
-  */
+    console.log('Baixando: '+link);
+    process.chdir('music/');
+    /* var linkmusic = link; */
+    var linkmusic = 'https://www.youtube.com/watch?v=wsrvmNtWU4E';
+    
+    child = exec('youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" --restrict-filenames ' + linkmusic,
+        function(error, stdout, stderr) {
+            var dados = stdout.split("\n");
+            var infoFolder = dados[7];
+            var localFile = infoFolder.split('estination: ');
+            var response = {local: "/music/" + localFile[1]}
+            res.jsonp(response);
+
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+
+    process.chdir('../');
+    
+});
+
+app.listen(process.env.PORT, function() {
+    console.log('Example app listening on port ' + process.env.PORT + '!');
 });
